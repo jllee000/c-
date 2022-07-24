787b72f0262a034248145b49d176fc51bfb4a912
jllee000 <101315059+jllee000@users.noreply.github.com>
2022-07-24 16:49:12 +0900

    Update 5. 문자열 압축.c

--- 3회차 소스코드_/5. 문자열 압축.c
+++ 3회차 소스코드_/5. 문자열 압축.c
@@ -7,7 +7,7 @@ int main() {
   char alpha[1000] = "";
   int cnt[26] ={0}; // 각 26개의 알파벳별 개수 (cnt[0]은 A의 개수 저장)
   fgets(alpha,1000,stdin);
-  int len = strlen(alpha)-1;
+  int len = strlen(alpha);
   for (int i=0; i<len; i++) {
     for (int j=0; j<26; j++) {
       if (alpha[i] == j+65) // 아스키코드 이용
